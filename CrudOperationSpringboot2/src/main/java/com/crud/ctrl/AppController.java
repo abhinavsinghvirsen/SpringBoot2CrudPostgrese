72236315893c72486c8f2e5f64982a1a35003d27
Abhinav <abhinavxeon@gmail.com>
2021-07-03 22:51:38 +0530

    added logger in sample application

--- CrudOperationSpringboot2/src/main/java/com/crud/ctrl/AppController.java
+++ CrudOperationSpringboot2/src/main/java/com/crud/ctrl/AppController.java
@@ -2,6 +2,8 @@ package com.crud.ctrl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crud.enty.User;
+import com.crud.repo.UserRepo;
 import com.crud.service.UserCrudService;
 
 
@@ -20,13 +23,22 @@ import com.crud.service.UserCrudService;
 @RestController
 @RequestMapping("/crudapp")
 public class AppController {
+	
+	Logger logger =LoggerFactory.getLogger(AppController.class);
+	
 	@Autowired
 	UserCrudService userCrudService;
+	
+
+	@Autowired
+	UserRepo userRepo; 
 	
 	
 	/* multi line comment*/
 	@PostMapping("/saveuser")
 	public String saveUser(@RequestBody User obj) {
+		logger.info("inside save api");
+		
 		
 		
 		return userCrudService.saveUser(obj);	
@@ -35,7 +47,11 @@ public class AppController {
 	@GetMapping("/getusers")
 	public List<User> getUser() {
 		
-		
+		logger.trace("trace");
+		logger.debug("debug");
+		logger.info("info");
+		logger.warn("warn");
+		logger.error("Erroro");
 		return userCrudService.getUsers();	
 	}
 	
--- CrudOperationSpringboot2/src/main/java/com/crud/service/UserCrudService.java
+++ CrudOperationSpringboot2/src/main/java/com/crud/service/UserCrudService.java
@@ -2,23 +2,28 @@ package com.crud.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.crud.ctrl.AppController;
 import com.crud.enty.User;
 import com.crud.repo.UserRepo;
 
 @Service
 public class UserCrudService {
 
-	
+	Logger logger =LoggerFactory.getLogger(UserCrudService.class);
 	@Autowired
 	UserRepo userRepo; 
 	
 	
 	public String saveUser(User usr) {
+		logger.trace("i am in side save user");
 		userRepo.save(usr);
-		
+		logger.warn("after save");
+		logger.info("inside service save api");
 		return "Sucess";
 		
 		
